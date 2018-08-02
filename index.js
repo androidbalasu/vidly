faacb7e64210b22fe52911ce2032939dbcd60371
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-01 20:19:39 -0500

    Update get and post methods to use mongodb.

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb://localhost/vidly')
+        .then(()=> console.log('Connected to vidly database...'))
+        .catch(error => console.log(error.message));
+
+        
 app.use(express.json());
 app.use('/api/genres', genres);
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
