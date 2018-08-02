cdf7156a55b63af1f36f4f26e0aa7fd38520beeb
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-01 21:30:26 -0500

    Add customers objects and api for the same.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const Joi = require('joi');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+//1 Connect to the database
+//2 Create a schema for the documents.
+//3 Create a model.
 
 mongoose.connect('mongodb://localhost/vidly')
         .then(()=> console.log('Connected to vidly database...'))
         .catch(error => console.log(error.message));
 
-        
+
 app.use(express.json());
 app.use('/api/genres', genres);
-
+app.use('/api/customers', customers);
 
 
 const port = process.env.PORT || 3000;
