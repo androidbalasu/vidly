e2b8ba6ac8f8054486a1b595cd9df07ddb95c7ef
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-07 16:26:11 -0500

    Install jsonwebtoken.   Generate a token and return it to the client.

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require ('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 router.post('/', async (req, res) => {
@@ -17,7 +18,11 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if(!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
+  const token = jwt.sign({
+     _id: user._id
+  }, 'jwtPrivateKey');
+
+  res.send(token);
   
 });
 
@@ -29,6 +34,5 @@ function validate(req) {
   
     return Joi.validate(req, schema);
   }
-
 
 module.exports = router;
