e75e863ab5bc0852c972fe087feda10c8a91d2ed
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-07 15:06:46 -0500

    Implement authentication logic.

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,6 @@ const _ = require('lodash');
 const bcrypt = require ('bcrypt');
 
 
-// router.get('/', async (req, res) => {
-//   let genres = await Genre.find().sort('name');
-//   res.send(genres);
-// });
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
@@ -26,30 +22,5 @@ router.post('/', async (req, res) => {
   res.send(_.pick(user, ['_id','name', 'email']));
 });
 
-// router.put('/:id', async(req, res) => {
-//   const { error } = validate(req.body); 
-//   if (error) return res.status(400).send(error.details[0].message);
-
-//   const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
-  
-//   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
-//    res.send(genre);
-// });
-
-// router.delete('/:id', async (req, res) => {
-
-//   const genre = await Genre.findByIdAndRemove(req.params.id);
-
-//   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-//   res.send(genre);
-// });
-
-// router.get('/:id', async (req, res) => {
-//   const genre = await Genre.findById(req.params.id);
-//   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-//   res.send(genre);
-// });
 
 module.exports = router;
