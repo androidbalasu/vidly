cf144c1ff519917de36ad3ec7a3dbcb7f8267468
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-08 15:40:15 -0500

    Using a jsonwebtoken to post new genres\customers etc..  Add a new middleware function to use the x-token, decode and set the token in the req.user object.

--- routes/genres.js
+++ routes/genres.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
+//Post method for posting a genre.
 router.post('/', async (req, res) => {
+  
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
