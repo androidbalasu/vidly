7a3f31c26a5f95eac62bb9be071a5c096a461465
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-08 14:24:54 -0500

    Using jwt to send the auth token.

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require ('bcrypt');
-
+const jwt = require ('jsonwebtoken');
+const config = require ('config');
 
 
 router.post('/', async (req, res) => {
@@ -19,7 +20,11 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ['_id','name', 'email']));
+  
+  const token = jwt.sign({
+    _id: user._id
+ }, config.get('jwtPrivateKey'));
+  res.header ('x-auth-token', token).send(_.pick(user, ['_id','name', 'email']));
 });
 
 
