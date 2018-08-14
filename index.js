7e3bed711a518606bcfab4c5f680f85549821217
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-14 12:43:20 -0500

    Extract configuration logic to its own file config.js

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const Joi = require('joi');
-
 const express = require('express');
 const app = express();
 require('./startup/routes')(app);
+const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const config = require('config');
+
 
 //1 Connect to the database
 //2 Create a schema for the documents.
 //3 Create a model. 
 require('./startup/logging')();
 require('./startup/db')();
+require('./startup/config')();
 
 const p = Promise.reject(new Error('Something failed miserably!'));
 p.then(()=> console.log('Done'));
-
-if (!config.get('jwtPrivateKey')){
-        console.error('FATAL ERROR: jwtPrivateKey is not defined');
-        process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
